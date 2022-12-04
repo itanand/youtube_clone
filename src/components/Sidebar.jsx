c2ea554953742478f359bda47c2036d70dacb36e
Anand Mohan <anand1505mohan@gmail.com>
2022-12-04 22:51:42 +0530

    done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
+import React from "react";
 import {Stack} from '@mui/material';
 
 import { categories } from '../utils/constants';
 
-const selectedCategory = 'New';
 
-const Sidebar = () =>  (
+const Sidebar = ({ selectedCategory, setSelectedCategory }) =>  (
     <Stack
       direction="row"
       sx={{
@@ -17,7 +17,8 @@ const Sidebar = () =>  (
       }}>
         {categories.map((category)=> (
             <button
-            className='category-btn'
+            className="category-btn"
+            onClick={() => setSelectedCategory(category.name)}
             style={{
                     background: category.name === selectedCategory && '#FC1503',
                     color: 'white'
